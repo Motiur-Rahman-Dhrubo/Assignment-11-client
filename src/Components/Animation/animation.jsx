f1e5b0ebd5c022f5d29a02664e76a2e76b3b10d8
motiur-rahman-dhrubo <mdmotiurrahman383@gmail.com>
2025-02-12 16:38:31 +0600

    animation sec header color change

--- src/Components/Animation/animation.jsx
+++ src/Components/Animation/animation.jsx
@@ -4,7 +4,7 @@ import lottieCar from "../../assets/lottie/car.json";
 const animation = () => {
     return (
         <div className="mt-8 w-11/12 mx-auto border border-red-500 p-4 rounded-lg">
-            <h2 className="md:text-5xl text-4xl text-black italic text-center font-medium" style={{ textShadow: "2px 2px 4px rgba(255, 185, 22, 0.8)" }}>Alert!!</h2>
+            <h2 className="md:text-5xl text-4xl text-black italic text-center font-medium" style={{ textShadow: "2px 2px 4px blue" }}>Alert!!</h2>
             <div className="mt-5 flex md:flex-row flex-col items-center gap-5">
                 <Lottie animationData={lottieCar} className="w-1/2"></Lottie>
                 <div className="w-1/2">
